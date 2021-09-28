18e5c1fee254b88364fb6029e5f67b5161522c4a
Ruben <ruben.serrano@iesdoctorbalmis.com>
2021-09-28 14:56:03 +0200

    Agregar archivos de proyecto.

---
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+
+namespace CaracteresLimitados
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Contenedor_TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+            Contador_TextBox.Text = Contenedor_TextBox.Text.Length + "/ 140";
+            if (Contenedor_TextBox.Text.Length >= 140)
+            {
+                Contenedor_TextBox.IsReadOnly = true;
+            }
+        }
+    }
+
+}
